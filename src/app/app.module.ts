b1ae0028ff1e14e00e4136386b2f3c2cbe59a4ab
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-11-30 16:55:16 +0100

    everthing work, also make it to input a table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+// import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from "@angular/common/http";
 
@@ -7,17 +8,23 @@ import { ProjectComponent } from './project/component/project.component';
 import { ProjectsComponent } from './projects/component/projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
+import { ProjectsTableComponent } from './projects/projects-table/projects-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectComponent,
     ProjectsComponent,
+    ProjectsTableComponent,
   ],
   imports: [
-    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule, MatTableModule, MatPaginatorModule, MatSortModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/projects/projects-table/projects-table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ProjectsTableDataSource, ProjectsTableItem } from './projects-table-datasource';
+
+@Component({
+  selector: 'app-projects-table',
+  templateUrl: './projects-table.component.html',
+  styleUrls: ['./projects-table.component.css']
+})
+export class ProjectsTableComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<ProjectsTableItem>;
+  dataSource: ProjectsTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new ProjectsTableDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}

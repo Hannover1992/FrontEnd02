6d34e2c5d52fbe9cd97b4d1f59e81b1d34f16546
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-12-13 17:15:28 +0100

    repeared paginator

--- src/app/projects/projects-table/projects-table-datasource.ts
+++ src/app/projects/projects-table/projects-table-datasource.ts
@@ -47,6 +47,7 @@ export class ProjectsTableDataSource extends DataSource<ProjectInterface> {
   private getPagedData(data: ProjectInterface[]): ProjectInterface[] {
     if (this.paginator) {
       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      console.log(data)
       return data.splice(startIndex, this.paginator.pageSize);
     } else {
       return data;
@@ -65,8 +66,6 @@ export class ProjectsTableDataSource extends DataSource<ProjectInterface> {
     return data.sort((a, b) => {
       const isAsc = this.sort?.direction === 'asc';
       switch (this.sort?.active) {
-        // case 'name': return compare(a.name, b.name, isAsc);
-        // case 'id': return compare(+a.id, +b.id, isAsc);
           case 'ID': return compare(+a.ID, +b.ID, isAsc);
           case 'Standort': return compare(+a.Standort, +b.Standort, isAsc);
           case 'Niederlassung': return compare(+a.Niederlassung, +b.Niederlassung, isAsc);

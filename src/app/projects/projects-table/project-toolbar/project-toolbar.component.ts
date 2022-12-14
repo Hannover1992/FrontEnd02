ebc2c85cacc9e58c705a559bf923b6255e0624ec
PatrykKrzyzanski <Hannover1992@googlemail.com>
2022-12-14 11:04:50 +0100

    make steky fotter, in them make the maximal viewer to 15, so do not need to scrool

---
+++ src/app/projects/projects-table/project-toolbar/project-toolbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-toolbar',
+  templateUrl: './project-toolbar.component.html',
+  styleUrls: ['./project-toolbar.component.css']
+})
+export class ProjectToolbarComponent {
+
+}

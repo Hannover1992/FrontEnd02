8cc88e3565f768edd93ba3bc30816c3068be1e6e
PatrykKrzyzanski <Hannover1992@googlemail.com>
2023-03-24 16:16:12 +0100

    bestellugn von notebook

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes} from "@angular/router";
+import {RouterModule} from "@angular/router";
+import {ProjectsTableComponent} from "./projects/projects-table/projects-table.component";
+
+const routes: Routes = [
+  {path: 'projects',  component: ProjectsTableComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }

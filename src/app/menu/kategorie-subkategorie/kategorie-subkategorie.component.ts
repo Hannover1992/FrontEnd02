3ef41a9e2c8c48927fd5f6a8905913d27314c94a
PatrykKrzyzanski <Hannover1992@googlemail.com>
2023-04-17 12:42:23 +0200

    leider fiasko, the table sicker druch ich werde mich jezt auf nav bar konzentrieren mussen

--- src/app/menu/kategorie-subkategorie/kategorie-subkategorie.component.ts
+++ src/app/menu/kategorie-subkategorie/kategorie-subkategorie.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class KategorieSubkategorieComponent {
 
+  animals = [
+    {
+      name: 'Vertebrates',
+      subcategories: [
+        {
+          name: 'Fishes',
+          items: ['Baikal oilfish', 'Bala shark', 'Ballan wrasse', 'Bamboo shark', 'Banded killifish']
+        },
+        {
+          name: 'Amphibians',
+          items: ['Sonoran desert toad', 'Western toad', 'Arroyo toad', 'Yosemite toad']
+        },
+        {
+          name: 'Reptiles',
+          items: ['Banded Day Gecko', 'Banded Gila Monster', 'Black Tree Monitor', 'Blue Spiny Lizard', 'Velociraptor']
+        },
+        { name: 'Birds', items: [] },
+        { name: 'Mammals', items: [] },
+      ]
+    },
+    {
+      name: 'Invertebrates',
+      subcategories: [
+        { name: 'Insects', items: [] },
+        { name: 'Molluscs', items: [] },
+        { name: 'Crustaceans', items: [] },
+        { name: 'Corals', items: [] },
+        { name: 'Arachnids', items: [] },
+        { name: 'Velvet worms', items: [] },
+        { name: 'Horseshoe crabs', items: [] },
+      ]
+    },
+  ];
+
+
 }

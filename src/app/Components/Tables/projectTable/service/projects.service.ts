efaf779697e4818c6fbf77022508627174db1d76
PatrykKrzyzanski <Hannover1992@googlemail.com>
2023-04-28 17:59:12 +0200

    projects service refactor deletet projects and replaced it whit BehaviorSubject

--- src/app/Components/Tables/projectTable/service/projects.service.ts
+++ src/app/Components/Tables/projectTable/service/projects.service.ts
@@ -9,7 +9,7 @@ import {Project} from "../../project/project";
 })
 
 export class ProjectsService {
-  projects: Project[];
+  // projects: Project[];
   selectedProject: BehaviorSubject<string> = new BehaviorSubject('3011');
   projects_subject!: BehaviorSubject<Project[]>;
   initialized: boolean = false;
@@ -17,12 +17,12 @@ export class ProjectsService {
   setProjects: ((newValue: any) => void) | undefined;
 
   constructor(public http: HttpClient ) {
-    this.projects = [];
+    // this.projects = [];
     this.getProjects();
 
-    this.selectedProject.subscribe(project => {
-      console.log(project)
-    });
+    // this.selectedProject.subscribe(project => {
+    //   console.log(project)
+    // });
 
     this.setProjects = (newValue) => {
       this.projects_subject.next(newValue);
@@ -34,7 +34,7 @@ export class ProjectsService {
     this.http.get<Project[]>(this.generateURL()).subscribe(
       (project_arr) => {
         this.projects_subject = new BehaviorSubject<Project[]>(project_arr);
-        this.projects = this.projects_subject.getValue();
+        // this.projects = this.projects_subject.getValue();
       });
   }
 

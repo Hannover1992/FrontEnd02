0474161397ffa1bfcfa745601f66b6327b586c45
PatrykKrzyzanski <Hannover1992@googlemail.com>
2023-04-28 18:02:13 +0200

    refactor projects service, deletet unntige funktion setProjects

--- src/app/Components/Tables/projectTable/service/projects.service.ts
+++ src/app/Components/Tables/projectTable/service/projects.service.ts
@@ -9,32 +9,18 @@ import {Project} from "../../project/project";
 })
 
 export class ProjectsService {
-  // projects: Project[];
   selectedProject: BehaviorSubject<string> = new BehaviorSubject('3011');
   projects_subject!: BehaviorSubject<Project[]>;
   initialized: boolean = false;
 
-  setProjects: ((newValue: any) => void) | undefined;
-
   constructor(public http: HttpClient ) {
-    // this.projects = [];
     this.getProjects();
-
-    // this.selectedProject.subscribe(project => {
-    //   console.log(project)
-    // });
-
-    this.setProjects = (newValue) => {
-      this.projects_subject.next(newValue);
-    };
-
   }
 
   public getProjects() {
     this.http.get<Project[]>(this.generateURL()).subscribe(
       (project_arr) => {
         this.projects_subject = new BehaviorSubject<Project[]>(project_arr);
-        // this.projects = this.projects_subject.getValue();
       });
   }
 

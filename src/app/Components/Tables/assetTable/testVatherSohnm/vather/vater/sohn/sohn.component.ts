92c5beb140ec886d62fd135d7092ba262de4ccd0
Your Name <you@example.com>
2023-06-12 09:11:42 +0200

    ein beispiel fur inheritence inside of an Angualr Compoenent

---
+++ src/app/Components/Tables/assetTable/testVatherSohnm/vather/vater/sohn/sohn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sohn',
+  templateUrl: './sohn.component.html',
+  styleUrls: ['./sohn.component.css']
+})
+export class SohnComponent {
+
+}
---
+++ src/app/Components/Tables/assetTable/testVatherSohnm/vather/vater/vater.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vater',
+  templateUrl: './vater.component.html',
+  styleUrls: ['./vater.component.css']
+})
+export class VaterComponent {
+
+}

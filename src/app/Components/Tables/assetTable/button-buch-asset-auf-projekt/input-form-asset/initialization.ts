e8dd2de771cd8ef10b68a29979661ae768d06454
Your Name <you@example.com>
2023-06-13 17:48:57 +0200

    refactor

---
+++ src/app/Components/Tables/assetTable/button-buch-asset-auf-projekt/input-form-asset/initialization.ts
@@ -0,0 +1,59 @@
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+export class Initialization {
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+  }
+
+  initForm() {
+    return this.fb.group({
+      asset_details: this.initAssetDetails(),
+      asset_numbers: this.initAssetNumbers(),
+      date_info: this.initDateInfo(),
+    });
+  }
+
+  initAssetDetails(): FormGroup {
+    return this.fb.group({
+      firma: [''],
+      artikelname: [''],
+      model: [''],
+      zustand: [''],
+      beschreibung: [''],
+    });
+  }
+
+  initAssetNumbers(): FormGroup {
+    return this.fb.group({
+      menge: [1, Validators.compose([
+        Validators.required,
+        Validators.pattern('^[0-9]*$'),
+        this.positiveNonZero // Make sure you have defined this custom validator function
+      ])],
+      preis: [''],
+      Inventarnummer: [0],
+      anlagenummer: [''],
+      serriennummer: [''],
+    });
+  }
+
+
+  positiveNonZero(control: AbstractControl) {
+    if (control.value <= 0) {
+      return {nonPositiveOrZero: true};
+    }
+    return null;
+  }
+
+  initDateInfo(): FormGroup {
+    return this.fb.group({
+      einkaufs_datum: [new Date()],
+      edit_date: [new Date()],
+      belegt_von: [new Date()],
+      belegt_bis: [new Date()],
+    });
+  }
+
+}
--- src/app/Components/Tables/assetTable/button-buch-asset-auf-projekt/input-form-asset/input-artikle-form.component.ts
+++ src/app/Components/Tables/assetTable/button-buch-asset-auf-projekt/input-form-asset/input-artikle-form.component.ts
@@ -7,65 +7,9 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 import {ProjectArticle} from "../../Interface/projectArticle";
 import {AssetTableService} from "../../../../Services/asset-table.service";
 import {Article} from "../../Interface/article";
+import {Initialization} from "./initialization";
 
 
-export class InputArtieklInitializiere{
-
-  constructor(
-    private fb: FormBuilder,
-  ) {
-  }
-
-  initForm() {
-    return this.fb.group({
-      asset_details: this.initAssetDetails(),
-      asset_numbers: this.initAssetNumbers(),
-      date_info: this.initDateInfo(),
-    });
-  }
-
-  initAssetDetails(): FormGroup {
-    return this.fb.group({
-      firma: [''],
-      artikelname: [''],
-      model: [''],
-      zustand: [''],
-      beschreibung: [''],
-    });
-  }
-
-  initAssetNumbers(): FormGroup {
-    return this.fb.group({
-      menge: [1, Validators.compose([
-        Validators.required,
-        Validators.pattern('^[0-9]*$'),
-        this.positiveNonZero // Make sure you have defined this custom validator function
-      ])],
-      preis: [''],
-      Inventarnummer: [0],
-      anlagenummer: [''],
-      serriennummer: [''],
-    });
-  }
-
-
-  positiveNonZero(control: AbstractControl) {
-    if (control.value <= 0) {
-      return {nonPositiveOrZero: true};
-    }
-    return null;
-  }
-
-  initDateInfo(): FormGroup {
-    return this.fb.group({
-      einkaufs_datum: [new Date()],
-      edit_date: [new Date()],
-      belegt_von: [new Date()],
-      belegt_bis: [new Date()],
-    });
-  }
-
-}
 
 @Component({
   selector: 'app-input-form-asset',
@@ -86,7 +30,7 @@ export class InputArtikelForm {
     private _snackBar: MatSnackBar,
     private assetTableService: AssetTableService,
   ) {
-    this.artikelForm =  new InputArtieklInitializiere(fb).initForm();
+    this.artikelForm =  new Initialization(fb).initForm();
   }
 
   onSubmit() {

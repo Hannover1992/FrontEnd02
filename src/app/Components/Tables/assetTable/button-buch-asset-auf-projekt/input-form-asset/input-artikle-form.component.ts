a69430380155bc1c29885cc366dc7af9fa4ec4e6
Your Name <you@example.com>
2023-06-13 17:44:50 +0200

    refactor

--- src/app/Components/Tables/assetTable/button-buch-asset-auf-projekt/input-form-asset/input-artikle-form.component.ts
+++ src/app/Components/Tables/assetTable/button-buch-asset-auf-projekt/input-form-asset/input-artikle-form.component.ts
@@ -89,55 +89,6 @@ export class InputArtikelForm {
     this.artikelForm =  new InputArtieklInitializiere(fb).initForm();
   }
 
-  initForm() {
-    this.artikelForm = this.fb.group({
-      asset_details: this.initAssetDetails(),
-      asset_numbers: this.initAssetNumbers(),
-      date_info: this.initDateInfo(),
-    });
-  }
-
-  initAssetDetails(): FormGroup {
-    return this.fb.group({
-      firma: [''],
-      artikelname: [''],
-      model: [''],
-      zustand: [''],
-      beschreibung: [''],
-    });
-  }
-
-  initAssetNumbers(): FormGroup {
-    return this.fb.group({
-      menge: [1, Validators.compose([
-        Validators.required,
-        Validators.pattern('^[0-9]*$'),
-        this.positiveNonZero // Make sure you have defined this custom validator function
-      ])],
-      preis: [''],
-      Inventarnummer: [0],
-      anlagenummer: [''],
-      serriennummer: [''],
-    });
-  }
-
-  initDateInfo(): FormGroup {
-    return this.fb.group({
-      einkaufs_datum: [new Date()],
-      edit_date: [new Date()],
-      belegt_von: [new Date()],
-      belegt_bis: [new Date()],
-    });
-  }
-
-
-  positiveNonZero(control: AbstractControl) {
-    if (control.value <= 0) {
-      return {nonPositiveOrZero: true};
-    }
-    return null;
-  }
-
   onSubmit() {
     let newProjectARticleFromForm = this.from_Form_to_ProjectArtickle();
     this.assetTableService.create_new_asset(newProjectARticleFromForm);

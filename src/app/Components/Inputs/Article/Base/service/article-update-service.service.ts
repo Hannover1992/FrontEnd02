d0bff8002479926dc7670d5e1ae2f59545807e75
Your Name <Hannover1992@googlemail.com>
2023-06-26 13:21:51 +0200

    bis hier hin seath good, aus menge kommt nicht mit und inventar nummer

---
+++ src/app/Components/Inputs/Article/Base/service/article-update-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Article} from "../../../../Interface/article";
+import {BehaviorSubject, Observer} from "rxjs";
+import {ProjectArticle} from "../../../../Interface/projectArticle";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleUpdateServiceService {
+
+  //behaviour Subject
+  currentProjectArticleForUpdate: BehaviorSubject<ProjectArticle> = new BehaviorSubject<ProjectArticle>({});
+
+  constructor() {
+  }
+}

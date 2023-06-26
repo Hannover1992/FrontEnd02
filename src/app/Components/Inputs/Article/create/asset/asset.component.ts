718541732ad1eb8a9b0daff76875ace5100f89f8
Your Name <Hannover1992@googlemail.com>
2023-06-26 19:16:44 +0200

    clean create for article and asset

---
+++ src/app/Components/Inputs/Article/create/asset/asset.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import {AssetFormDataService} from "./service/asset-form-data.service";
+
+@Component({
+  selector: 'app-asset',
+  templateUrl: './asset.component.html',
+  styleUrls: ['./asset.component.css']
+})
+export class AssetComponent implements  OnInit {
+  assetForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private assetFormDataService: AssetFormDataService,
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.assetForm = new intializationAsset(this.fb).initForm();
+    this.assetFormDataService.setForm(this.assetForm);
+  }
+
+}
+
+export class intializationAsset{
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  initForm(){
+    return this.fb.group({
+      ID: [''],
+      Inventarnummer: ['']
+    });
+  }
+
+}
---
+++ src/app/Components/Inputs/Article/create/asset/service/asset-form-data.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+import {FormGroup} from "@angular/forms";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AssetFormDataService {
+  private formData = new BehaviorSubject<any>({});
+  constructor() { }
+
+
+  setForm(form: FormGroup): void {
+    form.valueChanges.subscribe(data => this.formData.next(data));
+  }
+
+  getFormData(): Observable<any> {
+    return this.formData.asObservable();
+  }
+}
+//toDo: future improvement
+
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject, Observable } from 'rxjs';
+// import { FormGroup } from '@angular/forms';
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class FormDataService {
+//   private formData = new BehaviorSubject<any>({});
+//
+//   constructor() { }
+//
+//   setForm(form: FormGroup): void {
+//     form.valueChanges.subscribe(data => this.formData.next(data));
+//   }
+//
+//   getFormData(): Observable<any> {
+//     return this.formData.asObservable();
+//   }
+// }

20ded8518287752cd0e1e60e7cf16e32545e49ab
Your Name <Hannover1992@googlemail.com>
2023-07-11 14:25:43 +0200

    detail view simkarten

--- src/app/Components/Tables/Article/specialization/simkarten-table/simkarten-details-compose/simkarten-details-compose.component.ts
+++ src/app/Components/Tables/Article/specialization/simkarten-table/simkarten-details-compose/simkarten-details-compose.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-simkarten-details-compose',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./simkarten-details-compose.component.css']
 })
 export class SimkartenDetailsComposeComponent {
+
+  @Input() element: any;
 
 }

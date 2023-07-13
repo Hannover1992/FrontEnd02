29ada1252920706dde901b94243e222e13abe799
Your Name <Hannover1992@googlemail.com>
2023-07-13 14:38:02 +0200

    scaling

--- src/app/Components/Inputs/Article/2Asset/asset-compose-create.component.ts
+++ src/app/Components/Inputs/Article/2Asset/asset-compose-create.component.ts
@@ -3,10 +3,10 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-asset-compose-create',
   template: `
-    <app-article-input-compose></app-article-input-compose>
-    <app-menge></app-menge>
-    <app-asset></app-asset>
-    <app-control></app-control>
+      <app-article-input-compose></app-article-input-compose>
+      <app-menge></app-menge>
+      <app-asset></app-asset>
+      <app-control></app-control>
     `,
   styleUrls: ['../../../../shared_css/input.css']
 })
--- src/app/Components/Inputs/Article/3Simkarte/simkarten_compose.ts
+++ src/app/Components/Inputs/Article/3Simkarte/simkarten_compose.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-simkarten-compose-create',
   template: `
-    <app-article-input-compose></app-article-input-compose>
-    <app-menge></app-menge>
-    <app-simkarte></app-simkarte>
-    <app-simkarten-control></app-simkarten-control>
+    <div class="input_container">
+      <app-article-input-compose></app-article-input-compose>
+      <app-menge></app-menge>
+      <app-simkarte></app-simkarte>
+      <app-simkarten-control></app-simkarten-control>
+    </div>
   `,
   styleUrls: ['../../../../shared_css/input.css']
 })
